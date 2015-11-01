1a8f845b93a489242dc8f22813d95b47b542c406
VyshakhUnni <vyshakh.unnikrishnan@gmail.com>
2015-11-01 14:28:44 +0100

    Update Error.m

--- worksheet1/Error.m
+++ worksheet1/Error.m
@@ -1,7 +1,9 @@
-function f = Error(dt, approximations, analytical)
-sum = 0;
-for i = 1:length(approximations) % optimise?
-    sum = sum + (approximations(i) - analytical(i))^2;
-end
-f = sqrt(dt * 0.2 * sum);
+function f = Error(result,  dt, t_end)
+    sum = 0;
+    n = t_end / dt;
+    BestApprox  = Analytical(linspace(0,t_end,n ));
+    for i = 1:n
+        sum = sum + (result(i) - BestApprox(i))^2;
+    end
+    f = sqrt(dt * 0.2 * sum);
 end
