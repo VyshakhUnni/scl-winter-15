6976feb6e840829a1a7fd95721f01d9c8706f365
Will Parker <willparker@will-mbp.speedport_w_921v_1_39_000>
2015-11-14 13:18:00 +0100

    revert to my code.

--- worksheet2/implicit_euler.m
+++ worksheet2/implicit_euler.m
@@ -3,8 +3,6 @@ n = t_end / dt;
 f = zeros(1, n);
 f(1) = f0;
 for i = 1:n,
-    nextStepEquation = @(y)(y - f(i) - dt * func(y));
-    nextStepDerivative = @(y)(1 - dt * func_deriv(y));
-    f(i + 1) = f(i) + dt * func(newton(f(i), nextStepEquation, nextStepDerivative, tolerance));
+    f(i + 1) = f(i) + dt * func(newton(f(i), func, func_deriv, tolerance));
 end
 end
