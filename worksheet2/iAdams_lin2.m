b96b4d68fda86012bfcc5eafcdd970d0dd5f9989
nasay <nsay92@gmail.com>
2015-11-16 08:09:06 +0100

    fix adams-2

--- worksheet2/iAdams_lin2.m
+++ worksheet2/iAdams_lin2.m
@@ -2,13 +2,13 @@ function result = iAdams_lin2(f, df, y0, dt, t_end)
 
 % number of steps
 n = t_end / dt;
-result = zeros(1, n);
+result = NaN(1, n + 1);
 result(1) = y0;
 
 for i = 1:n
-    yn = result(i);
-    nextStepEquation = @(y)(y - yn - dt * .5 * (7 * (1 - yn / 10) * yn + 7 * (1 - yn / 10) * y));
-    nextStepDerivative = @(y)(1 + dt * .5 * 7 * yn / 10);
+    y_n = result(i);
+    nextStepEquation = @(y)(y - y_n - dt * .5 * (7 * (1 - y_n / 10) * y_n + 7 * (1 - y_n / 10) * y));
+    nextStepDerivative = @(y)(1 - dt * .5 * 7 * (1 - y_n / 10));
     result(i + 1) = newton(nextStepEquation, nextStepDerivative, result(i));
 end
 end
