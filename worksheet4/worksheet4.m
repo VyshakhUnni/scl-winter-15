1653b005617033d6059cf3a1e2534b0bc9440b9c
nasay <nsay92@gmail.com>
2015-12-13 18:53:04 +0100

    fix plotting + archive

--- worksheet4/worksheet4.m
+++ worksheet4/worksheet4.m
@@ -5,15 +5,25 @@ nodes_numbers = [ 3 7 15 31 ];
 
 t_end = 1;
 
-for t = t_list
-    k = 1;
-    figure('Name', strcat('Explicit Euler t=', num2str(t)));
-    for m = nodes_numbers
-        [N_x, N_y, N, h_x, h_y] = setVariables(m);
-        temperature_0 = ones(1, N);
-        
-        for dt = dt_list
-            result = eEuler(@euler_equation, temperature_0, dt, t_end, N_x, N_y);
+figure1 = figure('Name', 'Explicit Euler t = 0.125');
+figure2 = figure('Name', 'Explicit Euler t = 0.25');
+figure3 = figure('Name', 'Explicit Euler t = 0.375');
+figure4 = figure('Name', 'Explicit Euler t = 0.5');
+
+figures = [ figure1, figure2, figure3, figure4 ];
+
+% Plot's number in figure
+k = 1;
+
+for m = nodes_numbers
+    [N_x, N_y, N, h_x, h_y] = setVariables(m);
+    temperature_0 = ones(1, N);
+    
+    for dt = dt_list
+        result = eEuler(@euler_equation, temperature_0, dt, t_end, N_x, N_y);
+
+        for j = 1 : 4
+            t = t_list(j);
             
             result1 = [
                 zeros(1, N_x + 2);
@@ -21,24 +31,28 @@ for t = t_list
                 zeros(1, N_x + 2)
                 ];
             
+            figure(figures(j));
             subplot(4, 7, k);
+            
             surf(0 : h_x: 1, 0 : h_y : 1, result1);
-            k = k + 1;
         end
+        
+        k = k + 1;
     end
-    shg;
 end
+
+shg;
 
 figure('Name', 'Implicit Euler');
 k = 1;
 dt = 1/64;
-
-for t = t_list
-    for m = nodes_numbers
+for m = nodes_numbers
+    [N_x, N_y, N, h_x, h_y] = setVariables(m);
+    temperature_0 = ones(1, N);
+    
+    result = iEuler(temperature_0, dt, t_end, N_x, N_y);
+    for t = t_list
         [N_x, N_y, N, h_x, h_y] = setVariables(m);
-        temperature_0 = ones(1, N);
-
-        result = iEuler(temperature_0, dt, t_end, N_x, N_y);
         
         result1 = [
             zeros(1, N_x + 2);
